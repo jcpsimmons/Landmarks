f9e563137b5b01da66951c446ea6a3966f3a0dda
Josh Simmons MBP <jcpsimmons@gmail.com>
2022-11-18 20:22:32 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Josh Simmons on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
